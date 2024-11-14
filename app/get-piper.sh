c132f66b5e1cc7797aa4457c4503f0191a08f9d7
ZakariaAboudia <aboudiazakaria@gmail.com>
2024-11-14 22:24:13 +0000

    Update installation instruction to download piper in container

---
+++ app/get-piper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://github.com/rhasspy/piper/releases/download/2023.11.14-2/piper_linux_x86_64.tar.gz \
+&& tar -xvzf piper_linux_x86_64.tar.gz \
+&& rm piper_linux_x86_64.tar.gz
